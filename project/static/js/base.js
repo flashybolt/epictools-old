3c724e592451b3e9c926a0135b30288c6eecd319
Samuel C <wodaguge369@gmail.com>
2021-07-07 12:49:27 -0400

    Navbar phase 1

--- project/static/js/base.js
+++ project/static/js/base.js
@@ -1,3 +1,14 @@
 $(function() {
   $('.nav-content').hide();
+  $('#menu-icon').on('click', function() {
+    if ($(this).attr('uk-icon') == "icon: menu; ratio:1.5") {
+      $(this).hide().attr('uk-icon', 'icon: close; ratio:1.5').fadeIn(200);
+      $('.nav-content').fadeIn(200);
+      $('.nav-bar').addClass('nav-content-enlarged');
+    } else {
+      $(this).hide().attr('uk-icon', 'icon: menu; ratio:1.5').fadeIn(200);
+      $('.nav-content').fadeOut(200);
+      $('.nav-bar').removeClass('nav-content-enlarged');
+    }
+  });
 });
