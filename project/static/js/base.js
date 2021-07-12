82587be535b261fe0cfb5dcff017711defcba434
Samuel C <wodaguge369@gmail.com>
2021-07-12 13:55:58 -0400

    Navbar phase 2

--- project/static/js/base.js
+++ project/static/js/base.js
@@ -1,5 +1,15 @@
+/*
 $(function() {
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
   $('.nav-content').hide();
+  $('.content').css('margin-left', $('.nav-bar').innerWidth());
+  document.getElementsByClassName('content')[0].style.width = 'calc(100vw - ' + $('nav-bar').innerWidth() + 'px)';
   $('#menu-icon').on('click', function() {
     if ($(this).attr('uk-icon') == "icon: menu; ratio:1.5") {
       $(this).hide().attr('uk-icon', 'icon: close; ratio:1.5').fadeIn(200);
@@ -10,6 +20,11 @@ $(function() {
       $('.nav-content').fadeOut(200);
       $('.nav-bar').removeClass('nav-content-enlarged');
     }
+    if (document.getElementsByClassName('nav-content-enlarged')[0]) {
+      document.getElementsByClassName('content')[0].style.marginLeft = 'calc(30vh + 40px)';
+    } else {
+      document.getElementsByClassName('content')[0].style.marginLeft = 'calc(6vh + 40px)';
+    }
   });
-
 });
+*/
